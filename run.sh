b81060d048b71e111402b2cd9bfcefa64297d249
Mike Petersen <mike@odania-it.de>
2018-05-27 22:22:16 +0200

    Allow to set the credentials in several namespaces

--- run.sh
+++ run.sh
@@ -6,15 +6,24 @@ DOCKER_REGISTRY_SERVER=https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.co
 DOCKER_USER=AWS
 DOCKER_PASSWORD=`aws ecr get-login --region ${AWS_REGION} --registry-ids ${AWS_ACCOUNT} | cut -d' ' -f6`
 
-echo "Removing previous secret"
-kubectl delete secret aws-registry || true
+for namespace in ${NAMESPACES}
+do
+	echo
+	echo "Working in Namespace ${namespace}"
+	echo
+	echo "Removing previous secret in namespace ${namespace}"
+	echo kubectl --namespace=${namespace} delete secret aws-registry || true
 
-echo "Creating new secret"
-kubectl create secret docker-registry aws-registry \
-	--docker-server=$DOCKER_REGISTRY_SERVER \
-	--docker-username=$DOCKER_USER \
-	--docker-password=$DOCKER_PASSWORD \
-	--docker-email=no@email.local
+	echo "Creating new secret in namespace ${namespace}"
+	echo kubectl create secret docker-registry aws-registry \
+		--docker-server=$DOCKER_REGISTRY_SERVER \
+		--docker-username=$DOCKER_USER \
+		--docker-password=$DOCKER_PASSWORD \
+		--docker-email=no@email.local \
+		--namespace=${namespace}
+	echo
+	echo
+done
 
 echo "Patching default serviceaccount"
 echo kubectl patch serviceaccount default -p '{"imagePullSecrets":[{"name":"aws-registry"}]}'
